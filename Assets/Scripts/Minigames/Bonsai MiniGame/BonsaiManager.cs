d6d0c1b446a55f423c8cce176e8cc2f07514ad90
MartinReinBR <martin.blomquist@hotmail.com>
2022-01-20 12:43:22 +0100

    BingBong

--- Assets/Scripts/Minigames/Bonsai MiniGame/BonsaiManager.cs
+++ Assets/Scripts/Minigames/Bonsai MiniGame/BonsaiManager.cs
@@ -6,7 +6,7 @@ public class BonsaiManager : MonoBehaviour
     [SerializeField] private IntVariable _currentLeafAmount;
     private int _totalLeafAmount = 15; // make non static
     //private int _maxLeafAmount = 12; // make non static
-    private int _minLeafAmount = 9; // make non static
+    private int _minLeafAmount = 10; // make non static
 
     private void Awake()
     {
