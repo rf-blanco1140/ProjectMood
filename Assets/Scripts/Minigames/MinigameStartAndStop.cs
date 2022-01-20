a6561704adfe83b604bd75e3951e2f61ab5c93d3
MartinReinBR <martin.blomquist@hotmail.com>
2022-01-20 15:43:14 +0100

    No movement during mingame

--- Assets/Scripts/Minigames/MinigameStartAndStop.cs
+++ Assets/Scripts/Minigames/MinigameStartAndStop.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Player;
 using UnityEngine;
 
 public class MinigameStartAndStop : MonoBehaviour
 {
     public GameObject _cameraManager;
+    [SerializeField] private GameObject _PlayerBody;
 
     private void OnEnable()
     {
         Debug.Log("MiniGame Time!");
         _cameraManager.GetComponent<CameraManager>().SwapCamera();
+        _PlayerBody.GetComponent<PlayerMovement>().enabled = false;
     }
 
     private void OnDisable()
     {
         if (_cameraManager != null)
             _cameraManager.GetComponent<CameraManager>().SwapCamera();
+        _PlayerBody.GetComponent<PlayerMovement>().enabled = true;
     }
 }
 
