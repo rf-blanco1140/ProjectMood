998746bd4e43ba5daeecb51ae2f6442887c9c4f2
jeepeer <jespervski@gmail.com>
2022-01-20 11:29:36 +0100

    Added Camera Rotation

--- Assets/Scripts/Room Camera/CameraMoveTrigger.cs
+++ Assets/Scripts/Room Camera/CameraMoveTrigger.cs
@@ -4,25 +4,20 @@ public class CameraMoveTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject node;
     [SerializeField] private Vector3Variable _nodeValue;
+    [SerializeField] private Vector3Variable _nodeRotationValue;
     [SerializeField] private BoolVariable movingCamera;
     private Vector3 _nodePosition;
+    private Quaternion _nodeRotation;
     private void Awake()
     {
         _nodePosition = node.transform.position;
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            movingCamera.boolValue = true;
-            _nodeValue.Value = _nodePosition;
-        }
+        _nodeRotation = node.transform.rotation;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         movingCamera.boolValue = true;
         _nodeValue.Value = _nodePosition;
+        _nodeRotationValue.Value = _nodeRotation.eulerAngles;
     }
 }
