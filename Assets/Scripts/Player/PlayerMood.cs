ef55ba0bf0136354dbe5615b587369169db8b487
settet <63656551+settet@users.noreply.github.com>
2022-01-24 14:21:02 +0100

    audiofixes
    more testing

---
+++ Assets/Scripts/Player/PlayerMood.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMood : MonoBehaviour
+{
+
+    public bool isHappy, isNeutral, isSad;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
