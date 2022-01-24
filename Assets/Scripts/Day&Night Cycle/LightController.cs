e4c492318a0e887434170bf4a3cffdd68a96930f
jeepeer <jespervski@gmail.com>
2022-01-24 13:39:50 +0100

    day & night + lightcontroller

---
+++ Assets/Scripts/Day&Night Cycle/LightController.cs
@@ -0,0 +1,67 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    [SerializeField] private Color _colorA;
+    [SerializeField] private Color _colorB;
+
+    // rotate and change color deppending on rotation angle ?
+    // blend the colors togehter
+    // using timemanager somehow
+    
+    // rotate + color morning
+    // rotate + color evening
+
+    private void Update()
+    {
+        RotateLightSource();
+        ColorBlend();
+    }
+
+    private void MorningRotation()
+    {
+        Quaternion poopie;
+        quaternion pooooooop = quaternion.Euler(190,0,0);
+        poopie = Quaternion.RotateTowards(transform.rotation,pooooooop, 5 * Time.deltaTime );
+        transform.rotation = poopie;
+    }
+
+    private void EveningRotation()
+    {
+        Quaternion poopsss = Quaternion.Euler(90, 0, 0);
+        transform.rotation = poopsss;
+        quaternion euh = quaternion.Euler(190,0,0);
+        quaternion popiii = Quaternion.RotateTowards(transform.rotation, euh, 5 * Time.deltaTime);
+        transform.rotation = popiii;
+    }
+    
+    
+    
+    private void RotateLightSource()
+    {
+        //var roflmao = quaternion.Euler(180, 0, 0);
+        
+        transform.Rotate(.1f, 0, 0, Space.Self);
+        quaternion _maxRotation = Quaternion.Euler(180,0,0);
+
+        //transform.rotation = Vector3.Lerp(transform.rotation,_maxRotation,Time.deltaTime);
+        
+        if (transform.rotation == _maxRotation)
+        {
+            quaternion _startPos = Quaternion.Euler(0,0,0);
+            transform.rotation = _startPos;
+        } 
+        
+
+        // _startRotation = quaternion.Euler(0,0,0);
+        //quaternion _endRotation = quaternion.Euler(190,0,0);
+    }
+
+    private void ColorBlend()
+    {
+        var lightie = GetComponent<Light>();
+        lightie.color = Color.Lerp(_colorA, _colorB, Mathf.Pow(0,1)* Time.deltaTime); 
+        //lightie.color = _colorC;
+    }
+}
---
+++ Assets/Scripts/Day&Night Cycle/TimeManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    // a day is 24h, 1h = 2min
+    // a "day" = 6h( 12 min )
+    // color/light changes during the day?
+    // at the end of the day get a summary of what happened
+
+    private float _inGameHourInSeconds = 120f;
+    private float _debugTime = 2f;
+    private int _hoursInDay = 0;
+    private int _dayTimeEnds;
+    private int _nightTimeStarts;
+    private int _day = 1;
+    
+    private void Awake()
+    {
+        StartCoroutine(HourlyTimer());
+    }
+
+    private IEnumerator HourlyTimer()
+    {
+        HourManager();
+        _hoursInDay++;
+        
+        DayTime();
+        NightTime();
+        
+        yield return new WaitForSeconds(1);
+        StartCoroutine(HourlyTimer());
+    }
+
+    private void DayTime()
+    {
+        if (_hoursInDay <= _dayTimeEnds)
+        {
+            Debug.Log(HourManager());
+        }
+    }
+
+    private void NightTime()
+    {
+        if (_hoursInDay >= _nightTimeStarts)
+        {
+            Debug.Log(HourManager());
+        }
+    }
+
+    private int HourManager()
+    {
+        if (_hoursInDay >= 7)
+        {
+            _hoursInDay = 0;
+            DayManager();
+            return _hoursInDay;
+        }
+        return _hoursInDay;
+    }
+    private int DayManager()
+    {
+        if (_day >= 7)
+        {
+            _day = 1;
+            return _day;
+        }
+        _day++;
+        return _day;
+    }
+}
