a0e7fcc208b113bbbc4966edb4c42ab0f6aa0aab
settet <63656551+settet@users.noreply.github.com>
2022-01-26 10:20:25 +0100

    PauseMenu
    Fixed scale issue
    added font

--- Assets/Scripts/UI/UIOnHoverEvent.cs
+++ Assets/Scripts/UI/UIOnHoverEvent.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UIOnHoverEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
     Vector3 cachedScale;
-    public float sizeX;
-    public float sizeY;
-    public float sizeZ;
+   // public float sizeX;
+   // public float sizeY;
+    //public float sizeZ;
 
+    public Text m_Text;
 
     void Start()
     {
@@ -20,19 +22,20 @@ public class UIOnHoverEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitH
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-
-        transform.localScale = new Vector3(sizeX, sizeY, sizeZ);
+        m_Text.fontSize = 40;
+        //transform.localScale = new Vector3(sizeX, sizeY, sizeZ);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-
-        transform.localScale = cachedScale;
+        m_Text.fontSize = 25;
+        //transform.localScale = cachedScale;
     }
 
  public void resetText()
     {
-        transform.localScale = cachedScale;
+        //transform.localScale = cachedScale;
+        m_Text.fontSize = 25;
     }
    
 }
