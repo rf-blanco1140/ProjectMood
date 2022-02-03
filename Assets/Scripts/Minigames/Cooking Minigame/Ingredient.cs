cd3c50fc94c61e315c8275a89e592a506dc67d97
MartinReinBR <martin.blomquist@hotmail.com>
2022-02-03 12:11:38 +0100

    FoodMinigameThings

--- Assets/Scripts/Minigames/Cooking Minigame/Ingredient.cs
+++ Assets/Scripts/Minigames/Cooking Minigame/Ingredient.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Ingredient : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Sprite _uncookedSprite;
+    [SerializeField] private Sprite _cookedSprite;
+    [SerializeField] private Sprite _overcookedSprite;
+
+    private void OnEnable()
     {
-        
+        gameObject.GetComponent<SpriteRenderer>().sprite = _uncookedSprite;
+    }
+
+    public void NextSprite()
+    {
+        if(gameObject.GetComponent<SpriteRenderer>().sprite == _uncookedSprite)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = _cookedSprite;
+        }
+        else if(gameObject.GetComponent<SpriteRenderer>().sprite == _cookedSprite)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = _overcookedSprite;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
